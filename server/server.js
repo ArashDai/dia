3c19a26d3230447846b91893011c7eff9c5ec365
Arash Dai <arashdai@gmail.com>
2016-04-29 16:56:06 -0700

    compression

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var bodyParser= require('body-parser')
 var path = require('path');
 var app = express();
+var compress = require('compression');
 
+app.use(compress()); 
 
 app.use(express.static('client'));
 
